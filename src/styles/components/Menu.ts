3e3f64c072ecf8fac54cc7bdc2448a0f8b8f1443
Ricardo Paulo <ricadopaulo-jiros@outlook.com>
2021-06-21 22:59:34 -0300

    fix: z-index do menu

--- src/styles/components/Menu.ts
+++ src/styles/components/Menu.ts
@@ -69,7 +69,7 @@ export const Container = styled.nav<ContainerProps>`
 
             flex-direction: row;
             align-items: center;
-            z-index: 1;
+            z-index: 1000;
 
             >span:first-of-type {
                 margin-right: 1rem;
@@ -82,11 +82,11 @@ export const Container = styled.nav<ContainerProps>`
 
         >div.menu-shandow {
             display: block;
-            z-index: 0;
+            z-index: 999;
         }
 
         >ul {
-            z-index: 1;
+            z-index: 1000;
             height: 100%;
         }
 
